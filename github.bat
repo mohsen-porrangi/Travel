5c7bb94c604366e92cd285944d5ce3fe52e34369
mohsen <mohsen.porrangi@outlook.com>
2025-05-21 15:03:15 +0330

    Initial commit

--- github.bat
+++ github.bat
@@ -2,6 +2,7 @@ git init
 git add .
 git commit -m "Initial commit"
 git branch -M master
+git remote remove origin
 git remote add origin https://github.com/mohsen-porrangi/Travel.git
 git push -u origin master
 
